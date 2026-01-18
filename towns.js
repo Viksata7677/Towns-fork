87ffd83e4b360212cb21261f040543a2ee6b8def
nikolay_dimitrov256 <raystlin7@gmail.com>
2026-01-18 16:33:03 +0200

    added function showMessage in towns.js

--- towns.js
+++ towns.js
@@ -44,3 +44,11 @@ function addTown() {
     $('#towns').append($('<option>').text(townName));
     $('#result').text(townName + " added.");
 }
+
+function showMessage(msg) { 
+	$('#result').text(msg).css("display", "block"); 
+	
+	setTimeout(function () { 
+		$('#result').hide('blind', {}, 500); 
+	}, 3000); 
+}
